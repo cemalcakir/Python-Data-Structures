702e06da7805de857a51be6f4e10a711edde9ff6
cemalcakir <69013894+cemalcakir@users.noreply.github.com>
2021-05-21 20:23:11 +0300

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,7 +1,9 @@
 def bubble_sort(array):
+    "Sorts the array."
     last_unsorted = len(array) - 1
     unsorted = True
     while unsorted:
+        "If the array is already sorted we don't traverse the array again."
         unsorted = False
         for i in range(last_unsorted):
             if array[i] > array[i + 1]:
